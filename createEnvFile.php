f907c458f84b8664eb623681f4655e8544cecae5
saparot <coding@saparot.io>
2022-12-28 00:39:16 +0100

    feat: createEnvFile to create .env

---
+++ createEnvFile.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?php
+$configKeys = [
+    'APP_ENV',
+    'APP_DEBUG',
+    'APP_NAME',
+    'APP_URL',
+    'DB_CONNECTION',
+    'DB_DATABASE',
+    'DB_HOST',
+    'DB_PASSWORD',
+    'DB_PORT',
+    'DB_USERNAME',
+];
+
+$envFile = dirname(__FILE__) . '/.env';
+
+$data = "";
+foreach ($configKeys as $key) {
+    $data .= sprintf("%s=%s\n", $key, getenv($key));
+}
+
+file_put_contents($envFile,$data);
+echo "updated env file $envFile\n";
