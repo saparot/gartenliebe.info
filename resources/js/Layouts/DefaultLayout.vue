cf1aaba9462d3385c35d1fdec6d92e125c843a9f
saparot <coding@saparot.io>
2022-12-28 19:04:39 +0100

    feat: setup inertia with vite

---
+++ resources/js/Layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <Link href="/landingpage">Landing Page</Link>
+    <Link href="/news">News</Link>
+    <div v-if="flashMessageSuccess">
+        {{ flashMessageSuccess }}
+    </div>
+    <slot>Default</slot>
+</template>
+
+<script setup>
+import {Link, usePage} from '@inertiajs/inertia-vue3';
+import {computed} from 'vue';
+
+const page = usePage();
+const flashMessageSuccess = computed(() => page.props.value.flash.success || false);
+</script>
