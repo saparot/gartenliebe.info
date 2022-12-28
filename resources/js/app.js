244f2594cede5109fd873ddc5256c6d142c0d383
saparot <coding@saparot.io>
2022-12-28 19:03:14 +0100

    feat: added app.js to replace bootstrap, calling inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/inertia-vue3';
+import DefaultLayout from './Layouts/DefaultLayout.vue';
+import {ZiggyVue} from 'ziggy';
+
+createInertiaApp({
+    resolve: async (name) => {
+        const pages = import.meta.glob('./Pages/**/**/*Page.vue');
+        const page = await pages[`./Pages/${name}/IndexPage.vue`]();
+        page.default.layout = page.default.layout || DefaultLayout;
+
+        return page;
+    },
+    setup ({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+        .use(plugin)
+        .use(ZiggyVue)
+        .mount(el);
+    },
+});
