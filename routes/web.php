64bc410620235510853a192151c4fe8c950e5055
saparot <coding@saparot.io>
2022-12-28 19:06:40 +0100

    feat: register LandingPageController, NewsController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\NewsPageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +20,6 @@ Route::get('/', function () {
         'projectNamePublic' => 'Gartenliebe',
     ]);
 });
+
+Route::get('/landingpage', [LandingPageController::class, 'index']);
+Route::get('/news', [NewsPageController::class, 'index']);
