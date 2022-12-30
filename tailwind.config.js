808d65424c301583c9d69650f6c19412460c5c1d
Markus <46591021+saparot@users.noreply.github.com>
2022-12-30 08:58:57 +0100

    fix: adjust tailwind content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
       './storage/framework/views/*.php',
       './resources/views/**/*.blade.php',
-      './resources/js/**/*.vue',
+      './resources/js/**/**/*.vue',
   ],
   theme: {
     extend: {},
