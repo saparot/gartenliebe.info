2a741330e3b86a04fc7eab84f71d285d1572d65a
Markus <46591021+saparot@users.noreply.github.com>
2023-01-06 08:57:05 +0100

    fix: vite: define images folder as external

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             external: [
-                'image:static/images/*',
+                /^static\/images:.*/,
             ]
         },
     },
