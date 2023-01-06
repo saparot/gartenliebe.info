617eca8795b148629b6ca8ab7e802cc9f5c4c6bd
Markus <46591021+saparot@users.noreply.github.com>
2023-01-06 09:45:57 +0100

    fix: removed not required roll out options

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            external: [
-                /^static\/images:.*/,
-            ]
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
