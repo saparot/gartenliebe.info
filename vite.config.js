cb41518f78c33615fb960362de94afb143006c9c
Markus <46591021+saparot@users.noreply.github.com>
2023-01-06 08:55:00 +0100

    fix: vite: define images folder as external

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            external: [
+                'image:static/images/*',
+            ]
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
