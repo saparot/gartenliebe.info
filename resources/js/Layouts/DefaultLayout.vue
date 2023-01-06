e358f72cc9e003d2810619ae90f7f97fdc019c88
Markus <46591021+saparot@users.noreply.github.com>
2023-01-06 08:40:15 +0100

    feat: update base layout, show logged in user

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,34 +1,56 @@
 <template>
     <header class="bg-gray-200 dark:bg-gray-900 mb-4">
-        <div class="p-1 ">
-            <div class="h-9 flex w-full justify-center items-center flex-row">
+
+        <div class="flex w-full justify-between ">
+            <div class="w-1/3">
+                <!-- 1st box ------------->
+            </div>
+            <div class="w-1/3 flex justify-center items-center">
+                <!-- 2nd box ------------->
                 <div class="w-12">
                     <img class="max-w-full h-auto" src="/static/images/logo/logo.svg" alt="gartenliebe.info logo">
                 </div>
-                <div class="text-lime-700 font-bold">gartenliebe.info</div>
+                <div class="text-lime-700 font-bold">
+                    gartenliebe.info
+                </div>
             </div>
-            <div class="flex justify-around">
-                <Link class="hover:color-primary" :href="route('landingpage.index')">Pflanzen Lexikon</Link>
-                <Link class="hover:color-primary" :href="route('news.index')">News</Link>
-                <Link class="hover:color-primary" :href="route('news.index')">Blog</Link>
-                <Link class="hover:color-primary" :href="route('news.index')">Artikel</Link>
-                <Link class="hover:color-primary" :href="route('news.index')">Rezepte</Link>
-                <Link class="hover:color-primary" :href="route('login')">Login</Link>
+            <div class="w-1/3 flex justify-end pr-1">
+                <!-- 3rd box ------------->
+                <div class="ml-1">
+                    <!-- assigned by HandleInertiaRequests-->
+                    <div v-if="user">
+                        <div>{{ user.name }}</div>
+                        <Link class="hover:color-primary" :href="route('logout')" method="DELETE" as="button">Logout</Link>
+                    </div>
+                    <div v-else>
+                        <Link class="hover:color-primary" :href="route('login')">Login</Link>
+                    </div>
+                </div>
+            </div>
 
-            </div>
+        </div>
+
+        <div class="flex justify-around">
+
+            <Link class="hover:color-primary" :href="route('landingpage.index')">Pflanzen Lexikon</Link>
+            <Link class="hover:color-primary" :href="route('news.index')">News</Link>
+            <Link class="hover:color-primary" :href="route('news.index')">Blog</Link>
+            <Link class="hover:color-primary" :href="route('news.index')">Artikel</Link>
+            <Link class="hover:color-primary" :href="route('news.index')">Rezepte</Link>
+
         </div>
         <div class="h-0.5 bg-lime-700"></div>
     </header>
     <main>
         <section v-if="flashMessageSuccess" class="bg-lime-300 text-black">
-            {{flashMessageSuccess}}
+            {{ flashMessageSuccess }}
         </section>
         <section v-if="flashMessageWarning" class="bg-orange-300 text-black">
-            {{flashMessageWarning}}
+            {{ flashMessageWarning }}
         </section>
 
         <section v-if="flashMessageError" class="bg-red-900 text-white">
-            {{flashMessageError}}
+            {{ flashMessageError }}
         </section>
 
         <slot/>
@@ -43,5 +65,7 @@ const page = usePage();
 const flashMessageSuccess = computed(() => page.props.value.flashMessage.success);
 const flashMessageError = computed(() => page.props.value.flashMessage.error);
 const flashMessageWarning = computed(() => page.props.value.flashMessage.warning);
+
+const user = computed(() => page.props.value.user);
 
 </script>
