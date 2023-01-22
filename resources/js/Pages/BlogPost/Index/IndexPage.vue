b218b19bd8767af80a55f0d0452a4a02512ec96c
Markus <46591021+saparot@users.noreply.github.com>
2023-01-22 15:00:31 +0100

    feat: rename blog controller -> blogPost controller

---
+++ resources/js/Pages/BlogPost/Index/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <h1 class="text-6xl font-normal leading-normal mt-0 mb-2 text-fuchsia-800">Latest blog posts</h1>
+
+
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <BlogPostBox v-for="blog in blogs" :key="blog.id" :blog="blog"/>
+    </div>
+
+</template>
+
+<script setup>
+
+import BlogPostBox from '/resources/js/Elements/BlogPost/BlogPostBox.vue';
+
+const props = defineProps({
+    blogs: Array,
+});
+</script>
