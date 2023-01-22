2707e8e1bdb9e914c3e1dbfb48aa244368bfb410
Markus <46591021+saparot@users.noreply.github.com>
2023-01-22 15:00:56 +0100

    feat: added BlogPostBox

---
+++ resources/js/Elements/BlogPost/BlogPostBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <Box>
+        <div class="text-gray-500 font-medium mb-1">
+            {{ blog.title_de }}
+        </div>
+        <div>
+            {{ blog.content_de }}
+        </div>
+    </Box>
+
+</template>
+
+<script setup>
+
+import Box from '/resources/js/UI/PurpleOlives/Box.vue';
+
+const props = defineProps({
+    blog: Object,
+});
+</script>
