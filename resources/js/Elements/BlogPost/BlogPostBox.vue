0ebade93280ad503a3a004607b91129fe39bfbc9
Markus <46591021+saparot@users.noreply.github.com>
2023-02-04 07:49:38 +0100

    feat(BlogPost): basic layout refinement

--- resources/js/Elements/BlogPost/BlogPostBox.vue
+++ resources/js/Elements/BlogPost/BlogPostBox.vue
@@ -1,16 +1,28 @@
 <template>
     <Box>
-        <div class="text-gray-500 font-medium mb-1">
-            {{ blog.title_de }}
-        </div>
-        <div>
-            {{ blog.content_de }}
-        </div>
+        <div class="flex overflow-hidden max-h-56">
 
-        <div class="flex">
-            <a :href="route('blog.show', {blog: blog})" class="button-primary">show</a>
-            <a :href="route('blog.edit', {blog: blog})" class="button-secondary">edit</a>
+            <img src="/images/random/random1.jpg"
+                 alt="garden picture"
+                 class="object-cover w-2/5 border-r2 border-lime-700"
+            >
 
+            <div class="bg-gray-200 dark:bg-gray-900 w-full flex flex-col justify-between py-0.5 pl-1">
+                <div class="dark:text-lime-800 font-bold mb-1">
+                    {{ blog.title_de }}
+                </div>
+                <div class="relative">
+                    <div class="overflow-hidden">
+                        {{ blog.content_de }}
+                    </div>
+                </div>
+
+                <div class="flex justify-end">
+                    <a :href="route('blog.show', {blog: blog})" class="button-primary">show</a>
+                    <a :href="route('blog.edit', {blog: blog})" class="button-secondary">edit</a>
+
+                </div>
+            </div>
         </div>
     </Box>
 
