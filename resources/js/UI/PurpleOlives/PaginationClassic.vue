15a0d17edf33d04569994472db8bb1006d7b692f
Markus <46591021+saparot@users.noreply.github.com>
2023-02-04 08:39:58 +0100

    feat(Pagination): added classic simple Pagination

---
+++ resources/js/UI/PurpleOlives/PaginationClassic.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex gap-1">
+        <Link
+            v-for="(link, index) in links"
+            :key="index" class="box"
+            :href="link.url"
+            :class="{
+                'hover:bg-primary hover:text-white':true,
+                'bg-secondary text-white dark:bg-secondary dark:hover:text-inherit': link.active
+            }"
+            v-html="link.label"
+        />
+    </div>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/inertia-vue3';
+
+defineProps({links: Array});
+</script>
