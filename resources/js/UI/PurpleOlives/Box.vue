7f3bdac23ea984ecb973b02c635b1e42c6105670
Markus <46591021+saparot@users.noreply.github.com>
2023-02-04 10:04:19 +0100

    feat(compontes): adjust box color

--- resources/js/UI/PurpleOlives/Box.vue
+++ resources/js/UI/PurpleOlives/Box.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="border border-lime-700 dark:border-lime-700 rounded-md p-4 shadow-sm dark:text-gray-300">
+    <div class="border border-lime-700 dark:border-gray-900 border-2 rounded-md shadow-sm dark:text-gray-300">
         <slot/>
     </div>
 </template>
