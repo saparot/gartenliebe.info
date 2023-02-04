d896b9455dc796c2b3ead77b8b93d0ef36e2c541
Markus <46591021+saparot@users.noreply.github.com>
2023-02-04 10:51:42 +0100

    feat(routes): added routes

--- resources/js/lib/routes.js
+++ resources/js/lib/routes.js
@@ -1,6 +1,21 @@
+export const routeAccount = {
+    signUp: 'user-account.create',
+    store: 'user-account.store',
+    login: 'login',
+    logout: 'logout',
+
+};
+
 export const routeBlog = {
     list: 'blog.index',
     show: 'blog.show',
-    create: 'creator.blog.create',
-    edit: 'creator.blog.edit',
+    creator: {
+        list: 'creator.blog.index',
+        create: 'creator.blog.create',
+        store: 'creator.blog.store',
+        show: 'creator.blog.show',
+        edit: 'creator.blog.edit',
+        update: 'creator.blog.update',
+        delete: 'creator.blog.delete',
+    },
 };
