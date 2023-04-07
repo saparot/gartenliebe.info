a4016d42415ceeacd11bdf1b9b4ea242df679570
Markus <46591021+saparot@users.noreply.github.com>
2023-04-07 11:40:03 +0200

    feat(language): established key for fe: `languageIso`

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,6 +40,7 @@ class HandleInertiaRequests extends Middleware {
     public function share (Request $request): array {
         return array_merge(parent::share($request), [
             'projectName' => 'gartenliebe.info',
+            'languageIso' => 'de',
             'flashMessage' => [
                 'success' => $request->session()->get('flashMessageSuccess'),
                 'warning' => $request->session()->get('flashMessageWarning'),
