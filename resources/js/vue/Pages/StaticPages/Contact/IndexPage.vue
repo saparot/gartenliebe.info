657b70e66526379d5c01ea634012bf81d266920d
Markus <46591021+saparot@users.noreply.github.com>
2023-04-07 12:13:28 +0200

    feat(static-pages): added dummy template

---
+++ resources/js/vue/Pages/StaticPages/Contact/IndexPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1 class="headline-1">Contact</h1>
+<!--    <div>-->
+<!--        You can reach me best by email or using that contact form here. Do not hesitate to contact me with any questions you may have.-->
+<!--        <form action="">-->
+<!--            <div>-->
+<!--                <UserIcon class="h-4"/>-->
+<!--                <input type="text" name="name" placeholder="your name" disabled>-->
+
+<!--            </div>-->
+<!--            <div>-->
+<!--                <PencilIcon class="h-4"/>-->
+<!--                <input type="text" name="email" placeholder="your email" disabled>-->
+
+<!--            </div>-->
+<!--            <div>-->
+<!--                <EnvelopeIcon class="h-4"/>-->
+<!--                <textarea name="message" cols="30" rows="10" placeholder="your message here" disabled></textarea>-->
+
+<!--            </div>-->
+<!--            <div>-->
+
+<!--                <button type="submit" disabled class="button-primary">send</button>-->
+<!--            </div>-->
+<!--        </form>-->
+<!--    </div>-->
+
+
+</template>
+<script setup>
+import {EnvelopeIcon, UserIcon, PencilIcon} from '@heroicons/vue/24/solid';
+
+</script>
