dd1f9a5b669ac47b4bbe9ef505f057fa43de74b6
Markus <46591021+saparot@users.noreply.github.com>
2023-04-07 11:38:03 +0200

    boyscout(models): added typehints

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -7,6 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * App\Models\BlogPost
+ *
+ * @property int $id
+ * @property string $title_de
+ * @property string $content_de
+ * @property string $seo_keywords
+ * @property string $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\BlogPostImage[] $images
+ * @property-read int|null $images_count
+ * @property-read \App\Models\User|null $owner
+ * @method static \Database\Factories\BlogPostFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereContentDe($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereSeoKeywords($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereTitleDe($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class BlogPost extends Model {
 
     use HasFactory;
