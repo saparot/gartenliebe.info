d3a46ed1e4a3b635248aa7e61c5d724cf79590af
Markus <46591021+saparot@users.noreply.github.com>
2023-04-07 23:36:44 +0200

    feat(static-pages): added german trans files

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Dein Password wurde zurückgesetzt',
+    'sent' => 'Email mit Reset-link ging raus!',
+    'throttled' => 'Langsam. Bitte warte etwas bevor du es erneut probierst',
+    'token' => 'Der Token passt nicht. Lass dir eine neue Passwort Mail zukommen.',
+    'user' => "hm.",
+
+];
