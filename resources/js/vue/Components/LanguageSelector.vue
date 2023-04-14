08d7195eb4e872b52d29a6e2c806f6cb42b90b49
Markus <46591021+saparot@users.noreply.github.com>
2023-04-14 16:55:40 +0200

    feat(multilanguage): select language by flag (#9)

--- resources/js/vue/Components/LanguageSelector.vue
+++ resources/js/vue/Components/LanguageSelector.vue
@@ -1,20 +1,29 @@
 <template>
-    <div class="ml-4">
+    <div class="justify-items-center ml-4">
         <Link
-            class="inline-flex items-center px-1 pt-1 text-sm"
+            class=""
             :href="route('language.switch', { locale})"
             method="POST"
         >
-            <div v-if="localeGiven === 'de'">EN</div>
-            <div v-else>DE</div>
+            <div v-if="localeGiven === 'de'"><img :src="flagEnglish" alt="switch language to English" class="max-w-full h-10"></div>
+            <div v-else><img :src="flagGerman" alt="switch language to German" class="max-w-full h-10"></div>
         </Link>
     </div>
-    <div>{{ localeGiven }}</div>
+
+    <div v-if="localeGiven === 'de'">
+        <img :src="flagGerman" alt="switch language to German" class="max-w-full h-10">
+    </div>
+    <div v-else>
+        <img :src="flagEnglish" alt="switch language to English" class="max-w-full h-10">
+    </div>
+
 </template>
 
 <script setup>
 import {Link, usePage} from '@inertiajs/vue3';
 import {computed} from 'vue';
+import flagGerman from '/resources/images/flags/de.svg';
+import flagEnglish from '/resources/images/flags/gb.svg';
 
 const locale = computed(() => usePage().props.locale === 'en' ? 'de' : 'en');
 const localeGiven = computed(() => usePage().props.locale === 'en' ? 'en' : 'de');
