fbb884ac05b29bea1082bfb7df6ab3e015fb4027
Markus <46591021+saparot@users.noreply.github.com>
2023-05-20 09:33:23 +0200

    feat(gtm): access identifier by tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-K8P34Q7');</script>
+        })(window,document,'script','dataLayer',{{ env('GTM_IDENTIFIER') }});</script>
     @endenv
 
     @routes
@@ -21,7 +21,7 @@
 <body class="bg-white text-black dark:bg-gray-900 dark:text-gray-300 debug-screens dark:gradient-corner">
 @env('GTM_IDENTIFIER')
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8P34Q7" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env('GTM_IDENTIFIER') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 @endenv
 @inertia
 </body>
